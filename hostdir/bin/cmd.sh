a44fab59606ff29d87ada105bcda6d799f2e6ae7
viniciusvdias <vvsdias@gmail.com>
2025-03-28 17:51:25 -0300

    update

---
+++ hostdir/bin/cmd.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+tcpserver -v 0 12345 /app/hostdir/bin/tcpserver.sh
--- hostdir/bin/tcpserver.sh
+++ hostdir/bin/tcpserver.sh
@@ -3,7 +3,7 @@
 while true; do
   id="$((RANDOM % 21))"
   sentencesize="$((RANDOM % 10 + 1))"
-  sentence="$(zcat /usr/share/dict/100words.txt.gz | shuf -n$sentencesize | tr '\n' ' ')"
+  sentence="$(zcat /app/hostdir/data/100words.txt.gz | shuf -n$sentencesize | tr '\n' ' ')"
   echo "$id $sentence"
   randominterval="$((RANDOM % 3))"
   sleep $randominterval
